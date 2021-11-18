7bf6e2bf8bf92e36142c9a7b746ba7480a018361
Legrain Samuel <samuel.legrain@bstorm.be>
2021-11-18 08:50:24 +0100

    Correctif Exo Controleur + Amélioration

---
+++ I3.WAD21.MyFirstMVC/Controllers/ExoController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace I3.WAD21.MyFirstMVC.Controllers
+{
+    public class ExoController : Controller
+    {
+        public static string name { get; set; }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("{id}/MultiplierPar2")]
+        [Route("{id}/MultiplyPer2")]
+        public IActionResult MultiplierParDeux(int id)
+        {
+            return Json(id*2);
+        }
+
+        public IActionResult SaveName(string id)
+        {
+            name = id;
+            return RedirectToAction("ShowName",new { id = 10});
+        }
+
+        public IActionResult ShowName(int id)
+        {
+            if (name is null) return BadRequest("Veuillez d'abord sauvegarder le nom...");
+            List<string> result = new List<string>();
+            for (int i = 0; i < id; i++)
+            {
+                result.Add(name);
+            }
+            return Json(result);
+        }
+    }
+}

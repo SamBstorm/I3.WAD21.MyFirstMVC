36c518d58303600bbe6e6851186ddffb8c29822d
Legrain Samuel <samuel.legrain@bstorm.be>
2021-12-14 15:39:42 +0100

    Add LoginFormvValidation

--- I3.WAD21.MyFirstMVC/Controllers/DemoController.cs
+++ I3.WAD21.MyFirstMVC/Controllers/DemoController.cs
@@ -4,9 +4,11 @@ using I3.WAD21.MyFirstMVC.Handlers;
 using I3.WAD21.MyFirstMVC.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace I3.WAD21.MyFirstMVC.Controllers
@@ -44,16 +46,53 @@ namespace I3.WAD21.MyFirstMVC.Controllers
         {
             return View();
         }
+        ///// <summary>
+        ///// Action récupérant le formulaire dans un IFormCollection, permet de travailler les donnée d'un formulaire.
+        ///// ATTENTION : Signature doit être différente de l'affichage du formulaire, et être d'un HttpVerb différent, si l'affichage est en GET, la récupération est en POST (vérifier que la balise form contienne une méthode POST) : [HttpPost]
+        ///// </summary>
+        ///// <param name="formCollection"></param>
+        ///// <returns></returns>
+        //[HttpPost]
+        //public IActionResult Login(IFormCollection formCollection)
+        //{
+        //    return View();
+        //}
+        ///// <summary>
+        ///// Action récupérant le formulaire dans des paramètre typés, permet de travailler les donnée d'un formulaire. Attention chaque paramètre doit avoir le nom du name de l'input.
+        ///// ATTENTION : Signature doit être différente de l'affichage du formulaire, et être d'un HttpVerb différent, si l'affichage est en GET, la récupération est en POST (vérifier que la balise form contienne une méthode POST) : [HttpPost]
+        ///// </summary>
+        ///// <param name="formCollection"></param>
+        ///// <returns></returns>
+        //[HttpPost]
+        //public IActionResult Login(string email, string passwd)
+        //{
+        //    return View();
+        //}
+
         /// <summary>
-        /// Action récupérant le formulaire dans un IFormCollection, permet de travailler les donnée d'un formulaire.
+        /// Action récupérant le formulaire dans un model LoginForm, que j'ai créé moi-même, permet de travailler les donnée d'un formulaire.
         /// ATTENTION : Signature doit être différente de l'affichage du formulaire, et être d'un HttpVerb différent, si l'affichage est en GET, la récupération est en POST (vérifier que la balise form contienne une méthode POST) : [HttpPost]
         /// </summary>
         /// <param name="formCollection"></param>
         /// <returns></returns>
         [HttpPost]
-        public IActionResult Login(IFormCollection formCollection)
+        public IActionResult Login(LoginForm form)
         {
-            return View();
+            //ValidateLoginForm(form, ModelState);
+            if(!ModelState.IsValid) return View();
+            return RedirectToAction("Index", "Home");
         }
+
+        //private static void ValidateLoginForm(LoginForm form, ModelStateDictionary modelState)
+        //{
+        //    if (string.IsNullOrWhiteSpace(form.Email))
+        //        modelState.AddModelError(nameof(form.Email), "L'Email est obligatoire");
+        //    if (string.IsNullOrWhiteSpace(form.Passwd))
+        //        modelState.AddModelError(nameof(form.Passwd), "Le mot de passe est obligatoire");
+        //    if (form.Passwd is not null &&
+        //        !Regex.IsMatch(form.Passwd,
+        //        @"^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()])(?=\S+$).{8,20}$"))
+        //        modelState.AddModelError(nameof(form.Passwd), "Le mot de passe n'est pas correct.");
+        //}
     }
 }
---
+++ I3.WAD21.MyFirstMVC/Models/LoginForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace I3.WAD21.MyFirstMVC.Models
+{
+    public class LoginForm
+    {
+        [Required(ErrorMessage = "L'adresse email est obligatoire.")]
+        [EmailAddress(ErrorMessage ="L'adresse n'est au bon format.")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Le mot de passe est obligatoire.")]
+        [RegularExpression(@"^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()])(?=\S+$).{8,20}$",ErrorMessage = "Le mot de passe doit au minimum un nombre, une minuscule, une majuscule, un caractère parmis '@#$%^&-+=()', aucun espace blanc, compris entre 8 et 20 caractères.")]
+        public string Passwd { get; set; }
+        
+        // Si notre formulaire contient une liste de checkbox, et que ces dernières ont une valeur : alors chaque valeur cochée seront stocké dans un tableau
+        //public string[] Checkboxes { get; set; }
+    }
+}
